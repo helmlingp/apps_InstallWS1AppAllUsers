15ae047afdc60c987b4e5d4d4c57de11200d6c85
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-03-04 10:42:35 +1030

    Update installWS1AppAllUsers.ps1

--- installWS1AppAllUsers.ps1
+++ installWS1AppAllUsers.ps1
@@ -6,8 +6,8 @@
 
 # Test if WS1 App has been downloaded
 $WS1Apppath = "C:\Program Files (x86)\Airwatch\AgentUI\Resources\Bundle\AirWatchLLC.VMwareWorkspaceONE"
-$WS1Appfile = "668f4ce67ec547f3a39a59e031b8d07b.appxbundle"
-$WS1Applicense = "668f4ce67ec547f3a39a59e031b8d07b_License1.xml"
+$WS1Appfile = "1a8c85e4ecbc4740af719220e75405f8.appxbundle"
+$WS1Applicense = "1a8c85e4ecbc4740af719220e75405f8_License1.xml"
 if(Test-Path "$WS1Apppath\$WS1Appfile"){
     Add-AppxProvisionedPackage -Online -PackagePath $WS1Apppath\$WS1Appfile -LicensePath $WS1Apppath\$WS1Applicense;
 } else {
